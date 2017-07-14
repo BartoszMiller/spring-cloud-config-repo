6eaccc643ad86b6d579a00a3298c9ce43a6530f8
GFT\e-bzmr <miller.bartek@gmail.com>
2017-07-14 15:28:59 +0200

    Added new component to starting script

--- script.sh
+++ script.sh
@@ -8,3 +8,6 @@ java ${rds.client.options} -jar rds-client.jar ${rds.client.arguments}
 
 # Start RDS scheduler
 java ${rds.scheduler.options} -jar rds-scheduler.jar ${rds.scheduler.arguments}
+
+# Start RDS configuration server
+java ${rds.configuration.options} -jar rds-configuration.jar ${rds.configuration.arguments}
